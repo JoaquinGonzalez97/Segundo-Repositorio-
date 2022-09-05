9d9e02ee5376a4a8c5013e794b7ad9027ed1548a
Joaquin Gonzalez <GonzalezJoaquinEzequiel00@gmail.com>
2022-09-05 12:31:24 -0300

    Mejorando con JavaScript

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -1,3 +1,7 @@
 
 let boton = document.getElementById("btnMain")
 boton.onclick = () => {alert("Gracias por ponerse en contacto con nosotros")}
+
+let email   =  sessionStorage.getItem('email');
+
+console.log(typeof email);  
