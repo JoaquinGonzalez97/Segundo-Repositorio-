a3bcf26866f144af753c5ede8773f8fb6139fa20
Joaquin Gonzalez <GonzalezJoaquinEzequiel00@gmail.com>
2022-09-07 23:34:10 -0300

    Agregando Fetch

--- JavaScript/form.js
+++ JavaScript/form.js
@@ -1,4 +1,4 @@
-
+let url = 'https://jsonplaceholder.typicode.com/users/';
 let boton = document.getElementById("btnMain")
 
 boton.onclick = () => {alert("Gracias por ponerse en contacto con nosotros")}
@@ -17,3 +17,19 @@ btn.addEventListener('click', () => {
 let email   =  sessionStorage.getItem('email');
 
 console.log(typeof email);  
+
+
+fetch(url)
+    .then( response => response.json() )
+    .then( data => mostrarData(data) )
+    .catch( error => console.log(error) )
+
+const mostrarData = (data) => {
+    console.log(data)
+    let body = ""
+    for (var i = 0; i < data.length; i++) {      
+       body+=`<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`
+    }
+    document.getElementById('data').innerHTML = body
+    //console.log(body)
+}
